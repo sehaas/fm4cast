f0d819d5c17dd2f172df784e8437c53b895c260a
Sebastian Haas <sehaas@deebas.com>
2013-10-12 16:16:18 +0200

    js structure

---
+++ js/FM4Sender.js
@@ -0,0 +1,20 @@
+/*!
+ * FM4cast - FM4 ChromeCast receiver/sender
+ * http://sehaas.github.io/fm4cast
+ *
+ * Copyright 2013 Sebastian Haas (sehaas@deebas.com)
+ * Released under the MIT license
+ *
+ */
+
+( function( $ ) {
+
+	$.FM4Sender = function(element) {
+
+	};
+
+	$.FM4Sender.prototype = {
+
+	};
+
+}(jQuery));
