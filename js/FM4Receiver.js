829abce07f0d8b0b1b3331c48293501e21d94afd
Sebastian Haas <sehaas@deebas.com>
2013-10-12 19:14:04 +0200

    add sender

--- js/FM4Receiver.js
+++ js/FM4Receiver.js
@@ -1,5 +1,5 @@
 /*!
- * FM4cast - FM4 ChromeCast receiver/sender
+ * FM4cast - FM4 chromecase receiver/sender
  * http://sehaas.github.io/fm4cast
  *
  * Copyright 2013 Sebastian Haas (sehaas@deebas.com)
@@ -9,12 +9,30 @@
 
 ( function( $ ) {
 
-	$.FM4Receiver = function(element) {
 
+	$.FM4Receiver = function(element) {
+		$.FM4Receiver.tag = (element instanceof $) ? element : $(element);
 	};
+
+	$.FM4Receiver.receiver = null;
+	$.FM4Receiver.remoteMedia = null;
 
 	$.FM4Receiver.prototype = {
 
-	};
+		start : function() {
 
+			var receiver = $.FM4Receiver.receiver = new cast.receiver.Receiver(
+				fm4c.config.apikey, [cast.receiver.RemoteMedia.NAMESPACE], "", 5);
+
+			var remoteMedia = $.FM4Receiver.remoteMedia = new cast.receiver.RemoteMedia();
+			remoteMedia.addChannelFactory(
+				receiver.createChannelFactory(cast.receiver.RemoteMedia.NAMESPACE));
+
+			$(window).on("load", function() {
+				remoteMedia.setMediaElement($.FM4Receiver)
+			});
+
+			receiver.start();
+		},
+	};
 }(jQuery));
--- js/FM4Sender.js
+++ js/FM4Sender.js
@@ -1,5 +1,5 @@
 /*!
- * FM4cast - FM4 ChromeCast receiver/sender
+ * FM4cast - FM4 chromecast receiver/sender
  * http://sehaas.github.io/fm4cast
  *
  * Copyright 2013 Sebastian Haas (sehaas@deebas.com)
@@ -10,11 +10,79 @@
 ( function( $ ) {
 
 	$.FM4Sender = function(element) {
-
+		$.FM4Sender.tag = (element instanceof $) ? element : $(element);
 	};
 
+	$.FM4Sender.castApi = null;
+	$.FM4Sender.receiverMap = {};
+
 	$.FM4Sender.prototype = {
+		init : function() {
+			if (window.cast && window.cast.isAvailable) {
+				// Already initialized
+				initializeCastApi();
+			} else {
+				// Wait for API to post a message to us
+				$(window).on("message", function(event) {
+					if (event.source == window && event.data && event.data.source == "CastApi" && event.data.event == "Hello"){
+						initializeCastApi();
+					}
+				});
+			}
+		},
+
+		initializeCastApi : function() {
+			$.FM4Sender.castApi = castApi = new cast.Api();
+			castApi.addReceiverListener(fm4c.config.apikey, onReceiverList);
+		},
+
+		onReceiverList : function(list) {
+			$.FM4Sender.receiverMap = {};
+			var tag = $.FM4Sender.tag;
+
+			$.each(list, function(idx, val) {
+				$.FM4Sender.receiverMap[val.id] = val;
+				tag.append("<div id='cast_{1}' data-castid='{1}'>{0}</div>".format(val.name, val.id));
+			});
+			$("div", tag).on("click", onReceiverSelected);
+		},
+
+		onReceiverSelected : function() {
+			var $this = $(this);
+			var castId = $this.data("castid");
+			var receiver = $.FM4Sender.receiverMap[castId];
+			var launchRequest = new cast.LaunchRequest(fm4c.config.apikey, receiver);
+			launchRequest.parameters = '';
+
+			var loadRequest = new cast.MediaLoadRequest("http://mp3stream1.apasf.apa.at:8000/;");
+			loadRequest.autoplay = true;
+
+			$.FM4Sender.castApi.launch(launchRequest, function(status) {
+				if (status.status == 'running') {
+					currentActivityId = status.activityId;
+					$.FM4Sender.castApi.loadMedia(currentActivityId, loadRequest, launchCallback);
+				} else {
+					console.log('Launch failed: ' + status.errorString);
+				}
+			});
+		},
+
+		launchCallback : function(status) {
+			// noop
+		},
 
 	};
 
 }(jQuery));
+
+
+// helper function
+String.prototype.format = String.prototype.f = function() {
+    var s = this,
+        i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
